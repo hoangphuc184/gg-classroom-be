4535df832b4cff6b9ed2ad9f78758631806686f8
MrSooo <51949526+MrSooo@users.noreply.github.com>
2021-11-16 14:32:47 +0700

    -Delete old API with messy code

--- reference-only/api/models/classesModel.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, Sequelize) => {
-  const classes = sequelize.define("classes", {
-    name: {
-      type: Sequelize.STRING,
-    },
-  });
-
-  return classes;
-};
--- reference-only/api/routes/userRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../controllers/userController");
-
-/* GET classes list. */
-router.get("/", userController.list);
-
-module.exports = router;
