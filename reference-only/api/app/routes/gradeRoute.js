65fce0c0d7d6dcc12fa673d826f0260c5c968407
MrSooo <51949526+MrSooo@users.noreply.github.com>
2021-12-01 18:40:11 +0700

    +Upload list grade

--- reference-only/api/app/routes/gradeRoute.js
+++ reference-only/api/app/routes/gradeRoute.js
@@ -2,7 +2,9 @@ module.exports = (app) => {
   const grades = require("../controllers/gradeController");
   const { authJwt } = require("../middlewares");
 
+  const upload = require("../middlewares/upload");
   app.post("/api/grades", grades.create);
   app.get("/api/grades", grades.findAll);
   app.get("/api/download/grades", grades.downAll);
+  app.post("/api/upload/grades", upload.single("file"), grades.upload);
 };
