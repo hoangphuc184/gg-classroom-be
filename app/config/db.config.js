0e49fc51e63153dddafb63a0d11fe2d0d5cc49fe
Hoang Phuc <thienloc1220@gmail.com>
2022-01-13 02:27:12 +0700

    Change db config to remote DB

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,17 +1,31 @@
 //Local DB
-module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "Tuananh767256.",
-  DB: "classroom",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-};
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "Tuananh767256.",
+//   DB: "classroom",
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+// };
+
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "123456",
+//   DB: "c-resgistration",
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+// };
 
 // module.exports = {
 //   HOST: "localhost",
@@ -28,16 +42,16 @@ module.exports = {
 // };
 
 //Remote DB
-// module.exports = {
-//   HOST: "us-cdbr-east-04.cleardb.com",
-//   USER: "bac04cb4b36279",
-//   PASSWORD: "46e354df",
-//   DB: "heroku_58a1cdb5b2989bf",
-//   dialect: "mysql",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000,
-//   },
-// };
+module.exports = {
+  HOST: "us-cdbr-east-04.cleardb.com",
+  USER: "bac04cb4b36279",
+  PASSWORD: "46e354df",
+  DB: "heroku_58a1cdb5b2989bf",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
