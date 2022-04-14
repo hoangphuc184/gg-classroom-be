d9e871c023682833b33d9ab1621f9847050f0685
Hoang Phuc <47181743+hoangphuc184@users.noreply.github.com>
2022-04-14 17:00:51 +0700

    Update test.spec.js

--- app/_test_/test.spec.js
+++ app/_test_/test.spec.js
@@ -10,6 +10,6 @@ describe("Testing our nodeJS app", () => {
     it("Testing using Github Actions", () => {
         // exoecting 3 functions and receiving 3
         //  this test will pass unless the value change
-        expect(2).toBe(3);
+        expect(3).toBe(3);
     });
 });
