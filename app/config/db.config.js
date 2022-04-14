08c854536e44b61220df678f3a8518c6b1de21d3
Hoang Phuc <thienloc1220@gmail.com>
2022-04-14 12:01:51 +0700

    Add test

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -13,19 +13,19 @@
 //   },
 // };
 
-// module.exports = {
-//   HOST: "localhost",
-//   USER: "root",
-//   PASSWORD: "123456",
-//   DB: "c-resgistration",
-//   dialect: "mysql",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000,
-//   },
-// };
+module.exports = {
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "123456",
+  DB: "c-resgistration",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
 
 // module.exports = {
 //   HOST: "localhost",
@@ -42,16 +42,16 @@
 // };
 
 //Remote DB
-module.exports = {
-  HOST: "us-cdbr-east-04.cleardb.com",
-  USER: "bac04cb4b36279",
-  PASSWORD: "46e354df",
-  DB: "heroku_58a1cdb5b2989bf",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-};
+// module.exports = {
+//   HOST: "us-cdbr-east-04.cleardb.com",
+//   USER: "bac04cb4b36279",
+//   PASSWORD: "46e354df",
+//   DB: "heroku_58a1cdb5b2989bf",
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+// };
